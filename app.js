d4e082acb8faf93aa35072d67f3fc58bffd5976f
Ola Ishola <olaishola@hotmail.co.uk>
2020-03-31 23:46:56 +0100

    working on the app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-document.querySelector("#form-content").addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("#form-btn").onclick = function() {
-        let email = document.querySelector("#exampleInputEmail1").value;
+        let email = document.querySelector("#userName").value;
         let name = document.querySelector("#username").value;
         let location = document.querySelector("#location").value;
         let address = document.querySelector("#userAddress").value;
 
-        if (email.length < 0 || name.length < 0 || location.length < 0 || address.length < 0) {
+        if (email == "" || name == "" || location == "" || address == "") {
             alert("Please filled all the fields");
             return false;
         }
